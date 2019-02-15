8a22171d17a737a004c2ebdaad6c99e55ab02a61
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-02-15 07:57:28 -0600

    Update settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x*=7h==u-d*y_i2)ark_8f229&+iv6s_la)t5_o+3)*+&f57qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'areket.herokuapp.com']
 
