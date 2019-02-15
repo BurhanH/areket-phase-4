5ba78e27aa4500a8c4855d69adc9980da5cc51c0
BurhanH <urazalinov@gmail.com>
2019-02-14 21:00:34 -0600

    Updated static files 4

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+ADMIN_MEDIA_PREFIX = STATIC_ROOT + "/admin/"
 
 
 # Extra places for collectstatic to find static files.
